bbd7c3216ec1da10a022b242eba0867df25d9a0f
Marek Lubkiewicz <maronatwo@gmail.com>
2025-02-14 00:59:33 +0100

    kości

--- src/app/pages/main/games/tools/tools-routing.module.ts
+++ src/app/pages/main/games/tools/tools-routing.module.ts
@@ -6,9 +6,29 @@ import { ToolsPage } from './tools.page';
 const routes: Routes = [
   {
     path: '',
-    component: ToolsPage
+    component: ToolsPage,
+    children: [
+      {
+        path: 'dices',
+        loadChildren: () => import('../dices/dices.module').then( m => m.DicesPageModule)
+      },
+      {
+        path: 'timer',
+        loadChildren: () => import('../timer/timer.module').then( m => m.TimerPageModule)
+      },
+      {
+        path: 'cards',
+        loadChildren: () => import('../cards/cards.module').then( m => m.CardsPageModule)
+      },
+      {
+        path: '',
+        redirectTo: 'dices',
+        pathMatch: 'full'
+      }
+    ]
   }
 ];
+
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
